ee835b2a865dbe23f4cac5e0be1248f0fdaed312
Jay-Ubisse <joaquimubisse@gmail.com>
2022-12-13 16:59:08 +0200

    fixing some bugs

--- scripts/global.js
+++ scripts/global.js
@@ -1,82 +1,112 @@
 $(document).ready(function() {
 
-  /**Codigo para o menu (versao mobile) */
-
-  $("#menu-icon").hide();
-
-  if($(window).width() < 900){
-    $("#menu-icon").show();
-    $("#menu").hide();
-    $("#menu-icon").click(function () {
-      if ($("#menu").is(':hidden')) {
-        $("#menu").slideDown(300);
-      } else {
-        $("#menu").slideUp(300);
-      }
-    });
-  }
-
-  /**Codigo para mudanca de conteudo de pagina */
+    /**Ocultando os conteudos FAQ e Sobre nos*/
   $("#faq").hide();
   $("#about").hide();
 
-  $("#home").click(function() {
-    $("#about-us").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
+  /**Codigo para a versao mobile */
+
+  if($(window).width() < 1024){
+
+    $("#menu-icon").show();
+    $("#menu").hide();
+    $("#menu-icon").click(function () {
+      $("#menu").slideDown(300);
     });
-    $("#home").css( {
-      "color": "white",
-      "background-color": "#78290f"
-    });
-    $("#frequent").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
+    $("#exit-icon").click(function () {
+      $("#menu").slideUp(300);
     });
 
-    $("#slideshow").fadeIn(300);
-    $("#faq").hide();
-    $("#about").hide();
 
-  });
-  $("#about-us").click(function() {
-    $("#about-us").css( {
-      "color": "white",
-      "background-color": "#78290f"
+    /**Codigo para mudanca de conteudo de pagina */
+    $("#home").click(function() {
+      $("#menu").slideUp(300);
+      $("#slideshow").fadeIn(300);
+      $("#faq").hide();
+      $("#about").hide();
+  
     });
-    $("#home").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
+    $("#about-us").click(function() {
+      $("#menu").slideUp(300);
+      $("#slideshow").hide();
+      $("#faq").hide();
+      $("#about").fadeIn(300);
+  
     });
-    $("#frequent").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
-    });
-
-    $("#slideshow").hide();
-    $("#faq").hide();
-    $("#about").fadeIn(300);
-
-  });
-  $("#frequent").click(function() {
-    $("#about-us").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
-    });
-    $("#home").css( {
-      "color": "#78290f",
-      "background-color": "#ffecd1"
-    });
-    $("#frequent").css( {
-      "color": "white",
-      "background-color": "#78290f"
+    $("#frequent").click(function() {
+      $("#menu").slideUp(300);
+      $("#slideshow").hide();
+      $("#faq").fadeIn(300);
+      $("#about").hide();
+  
     });
 
-    $("#slideshow").hide();
-    $("#faq").fadeIn(300);
-    $("#about").hide();
 
-  });
+      /**Codigo para a versao desktop */
+  } else {
+
+    /**Codigo para mudanca de conteudo de pagina */
+
+    $("#home").click(function() {
+      $("#about-us").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+      $("#home").css( {
+        "color": "white",
+        "background-color": "#78290f"
+      });
+      $("#frequent").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+  
+      $("#slideshow").fadeIn(300);
+      $("#faq").hide();
+      $("#about").hide();
+  
+    });
+    $("#about-us").click(function() {
+      $("#about-us").css( {
+        "color": "white",
+        "background-color": "#78290f"
+      });
+      $("#home").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+      $("#frequent").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+  
+      $("#slideshow").hide();
+      $("#faq").hide();
+      $("#about").fadeIn(300);
+  
+    });
+    $("#frequent").click(function() {
+      $("#about-us").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+      $("#home").css( {
+        "color": "#78290f",
+        "background-color": "#ffecd1"
+      });
+      $("#frequent").css( {
+        "color": "white",
+        "background-color": "#78290f"
+      });
+  
+      $("#slideshow").hide();
+      $("#faq").fadeIn(300);
+      $("#about").hide();
+  
+    });
+  }
+
+
 });
 
   /**Codigo para o slide show */
